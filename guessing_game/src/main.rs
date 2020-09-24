47f91aa4c3ca0140210eef2ccc92de3bae87d3ef
marciocomp <marcio.comp@gmail.com>
2020-09-23 22:39:23 -0300

    Guessing game: the 3rd example of the book (Rust Land). It converts string to number to compare guess and secret_number

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,5 +1,6 @@
 use std::io;
 use rand::Rng;// used to random number
+use std::cmp::Ordering;
 
 fn main(){
     println!("Guess the number!");
@@ -15,5 +16,15 @@ fn main(){
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
+
+    let guess:u32 = guess.trim()
+        .parse().expect("Please type a number!");//It converts string to number -> u32
+
     println!("You guessed: {}",guess);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
---
+++ guessing_game/target/debug/deps/guessing_game-0e5d9996c9837f89.d
@@ -0,0 +1,5 @@
+/media/marcio/GENERAL DATA/intellij_idea/learning_rust_with_intellij/guessing_game/target/debug/deps/guessing_game-0e5d9996c9837f89: src/main.rs
+
+/media/marcio/GENERAL DATA/intellij_idea/learning_rust_with_intellij/guessing_game/target/debug/deps/guessing_game-0e5d9996c9837f89.d: src/main.rs
+
+src/main.rs:
