78493a48b53937e11c27be742b47c6b9a1b4368f
marciocomp <marcio.comp@gmail.com>
2020-09-23 22:16:30 -0300

    Hello world: Adding some comments and texts

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,3 +1,6 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, world!\n");//using double '\n'
+    println!("This is my first code in Rust using IntelliJ IDEA!\n");
+    println!("I've been used C in my classes during so many years and now \
+        I'd like to change the technique to teach my students.")
 }
