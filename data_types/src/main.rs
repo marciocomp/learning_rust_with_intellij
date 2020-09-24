d6ee89c8349f43154acf41f70a1e447c9d3ddf43
marciocomp <marcio.comp@gmail.com>
2020-09-24 11:36:48 -0300

    Data types: 3rd testing Integer types.

--- data_types/src/main.rs
+++ data_types/src/main.rs
@@ -4,4 +4,25 @@ fn main() {
     guess+=1.567;//just testing sum
     println!("Guess: {}",guess);
 
+    //scalar type -> single value -> four primary scalar types: integer, floating-point numbers,
+    //booleans, and characters
+
+    /************INTEGER TYPES************/
+    let decimal = 98_222;
+    let hex = 0xff;
+    let octal = 0o77;
+    let binary = 0b1111_0000;
+    let byte = b'A';
+
+    println!("Decimal: {}",decimal);// we can use annotation format to print as you want "to see"
+    println!("hex: {:0x}",hex);// We can use any format, for instance: Variable hex -> {:0b} -> It'll
+                                // print in binary form
+    println!("octal: {:0o}",octal);
+    println!("binary: {:0b}",binary);
+    println!("byte: {:b}",byte);
+
+    /************END INTEGER TYPES************/
+
+
+
 }
