4a418019fcee02dced58780451626c1c4cc07533
marciocomp <marcio.comp@gmail.com>
2020-09-24 14:07:46 -0300

    Data types: 4th testing Floating-point types.

--- data_types/src/main.rs
+++ data_types/src/main.rs
@@ -12,7 +12,7 @@ fn main() {
     let hex = 0xff;
     let octal = 0o77;
     let binary = 0b1111_0000;
-    let byte = b'A';
+    let byte: u8 = b'A';//only use u8 for "Byte" subtype
 
     println!("Decimal: {}",decimal);// we can use annotation format to print as you want "to see"
     println!("hex: {:0x}",hex);// We can use any format, for instance: Variable hex -> {:0b} -> It'll
@@ -23,6 +23,16 @@ fn main() {
 
     /************END INTEGER TYPES************/
 
+    /************FLOATING-POINT TYPES************/
+
+        let mut float_64 = 2.0; //the default is f64 -> Double precision
+        let mut float_32: f32 = 3.0; //single precision
+        float_64 += float_32;
+        float_32 = float_64+1.0;
+        println!("Sum of float 64: {}",float_64);
+        println!("Sum of float 32: {}",float_32);
+
+    /************END FLOATING-POINT TYPES************/
 
 
 }
