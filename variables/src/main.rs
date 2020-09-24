99cb8977c630b073e3c0df500ff9aa80bcae0b8f
marciocomp <marcio.comp@gmail.com>
2020-09-24 10:36:39 -0300

    Variables: 4th example -> Shadow variables.

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -3,9 +3,20 @@ fn main() {
     const MAX_POINTS: u32 = 100_000;// The underscore helps us to understand 100,000. We can use,
                                     // for instance, 1_000_000 that means 1,000,000. The underscore
                                     // does not influence in the final value of the constant.
-    let mut x = 5;//default type for integer: i32
+    let x = 5;//default type for integer: i32
     println!("The value of x is: {}", x);
 
-    x = MAX_POINTS;//trying constant MAX_POINTS
+    //Testing shadow variable
+    let x = MAX_POINTS;//trying constant MAX_POINTS and shadowing. The  new type of x is u32
+                            // because MAX_POINTS is u32
     println!("The value of x is: {}", x);
+
+    let x = x + 1; // 2nd shadow. Type of x is still u32 because last x was u32
+    let x = x * 2; // 3rd shadow
+
+    println!("The value of x is: {}", x);
+
+
+
+
 }
