08e1fa9dad19dcdddc02f5f2c9ec3769c8376d0b
marciocomp <marcio.comp@gmail.com>
2020-09-23 22:49:48 -0300

    Guessing game: I try to change some piece of code to read a integer in user input. It's having some warnings! :(

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,33 +1,49 @@
 use std::io;
 use rand::Rng;// used to random number
 use std::cmp::Ordering;
+use std::io::{Read, BufRead};
+use std::fs::read;
 
 fn main(){
     println!("Guess the number!");
 
     let secret_number= rand::thread_rng().gen_range(1, 101); //generate a number. No seed is used - immutable value
-
     // println!("The secret number is: {}",secret_number);
+    let mut count= 0;
     loop {
+        count+=1;
+        //    println!("Please input you guess: ");
+        println!("Please type the {}[st,nd,rd,th] magic number: ",count);
 
-        println!("Please input you guess.");
+        let mut magic: i32 = io::stdin().lock()//trying to input integer
+            .lines()
+            .next()
+            .expect("stdin should be available")
+            .expect("couldn't read from stdin")
+            .trim()
+            .parse()
+            .expect("input was not an integer");
+        println!("The magic number is: {}",magic);
 
-        let mut guess = String::new();//Storing Values with Variables - mutable
 
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("Failed to read line");
+        //    let mut guess = String::new();//Storing Values with Variables - mutable
 
-        let guess:u32 = match guess.trim()
-            .parse() {
-            Ok(num) => num,               //verifying invalid input
-            Err(_) => continue,
+        //  io::stdin()
+        //       .read_line(&mut guess)
+        // .expect("Failed to read line");
 
-        };//.expect("Please type a number!");//It converts string to number -> u32
+        // let guess:u32 = match guess.trim()
+        //     .parse() {
+        //     Ok(num) => num,               //verifying invalid input
+        //     Err(_) => {
+        //       println!("Please type a number!");
+        //       continue;
+        //     }
 
-        println!("You guessed: {}",guess);
+        // };//.expect("Please type a number!");//It converts string to number -> u32
 
-        match guess.cmp(&secret_number) {
+        // println!("You guessed: {}",guess);
+        match magic.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
             Ordering::Equal => {
