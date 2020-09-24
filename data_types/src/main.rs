576a9c159f08bf6e49d4e4c583ca6af2d179f702
marciocomp <marcio.comp@gmail.com>
2020-09-24 11:11:10 -0300

    Data types: 2nd making sum with result of parse.

--- data_types/src/main.rs
+++ data_types/src/main.rs
@@ -1,6 +1,7 @@
 fn main() {
 
     let mut guess: f32 = "42.09".parse().expect("Not a number!");// We must use type annotation e.g: f32
+    guess+=1.567;//just testing sum
     println!("Guess: {}",guess);
 
 }
