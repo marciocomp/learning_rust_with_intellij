82ffae9b8e2a1963d87b0524d38a51f02e8a4b8a
marciocomp <marcio.comp@gmail.com>
2020-09-24 10:25:45 -0300

    Variables: 3rd example, testing constants. It's adding some comments to understand later.

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,8 +1,11 @@
 fn main() {
-
+    //Testing CONSTANTS
+    const MAX_POINTS: u32 = 100_000;// The underscore helps us to understand 100,000. We can use,
+                                    // for instance, 1_000_000 that means 1,000,000. The underscore
+                                    // does not influence in the final value of the constant.
     let mut x = 5;//default type for integer: i32
     println!("The value of x is: {}", x);
 
-    x = 6;
+    x = MAX_POINTS;//trying constant MAX_POINTS
     println!("The value of x is: {}", x);
 }
