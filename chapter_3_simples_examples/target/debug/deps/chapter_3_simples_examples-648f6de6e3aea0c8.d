7c7b04be3245d86aacbcbc1ffac7c1516d1f6737
marciocomp <marcio.comp@gmail.com>
2020-09-24 09:38:17 -0300

    Chapter 3: 1st example with immutable variable. It has an compile error. It was to show how mutable/immutable works.

---
+++ chapter_3_simples_examples/target/debug/deps/chapter_3_simples_examples-648f6de6e3aea0c8.d
@@ -0,0 +1,5 @@
+/media/marcio/GENERAL DATA/intellij_idea/learning_rust_with_intellij/chapter_3_simples_examples/target/debug/deps/chapter_3_simples_examples-648f6de6e3aea0c8: src/main.rs
+
+/media/marcio/GENERAL DATA/intellij_idea/learning_rust_with_intellij/chapter_3_simples_examples/target/debug/deps/chapter_3_simples_examples-648f6de6e3aea0c8.d: src/main.rs
+
+src/main.rs:
---
+++ chapter_3_simples_examples/target/debug/deps/chapter_3_simples_examples-ed430377775779d7.d
@@ -0,0 +1,5 @@
+/media/marcio/GENERAL DATA/intellij_idea/learning_rust_with_intellij/chapter_3_simples_examples/target/debug/deps/chapter_3_simples_examples-ed430377775779d7: src/main.rs
+
+/media/marcio/GENERAL DATA/intellij_idea/learning_rust_with_intellij/chapter_3_simples_examples/target/debug/deps/chapter_3_simples_examples-ed430377775779d7.d: src/main.rs
+
+src/main.rs:
