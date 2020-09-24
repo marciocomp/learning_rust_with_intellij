3706e808c3b5f9eb0057312409cfe77217e9d431
marciocomp <marcio.comp@gmail.com>
2020-09-23 22:45:16 -0300

    Guessing game: the 4th example of the book (Rust Land). Add loop.

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -8,23 +8,28 @@ fn main(){
     let secret_number= rand::thread_rng().gen_range(1, 101); //generate a number. No seed is used - immutable value
 
     println!("The secret number is: {}",secret_number);
+    loop {
 
-    println!("Please input you guess.");
+        println!("Please input you guess.");
 
-    let mut guess = String::new();//Storing Values with Variables - mutable
+        let mut guess = String::new();//Storing Values with Variables - mutable
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    let guess:u32 = guess.trim()
-        .parse().expect("Please type a number!");//It converts string to number -> u32
+        let guess:u32 = guess.trim()
+            .parse().expect("Please type a number!");//It converts string to number -> u32
 
-    println!("You guessed: {}",guess);
+        println!("You guessed: {}",guess);
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
     }
 }
