5ab833b934644757790175426ca391c80054fef2
marciocomp <marcio.comp@gmail.com>
2020-09-25 08:42:26 -0300

    Functions: 1st example and commit by GitHub-Desktop.

---
+++ functions/target/debug/functions.d
@@ -0,0 +1,1 @@
+/media/marcio/GENERAL\ DATA/intellij_idea/learning_rust_with_intellij/functions/target/debug/functions: /media/marcio/GENERAL\ DATA/intellij_idea/learning_rust_with_intellij/functions/src/main.rs
