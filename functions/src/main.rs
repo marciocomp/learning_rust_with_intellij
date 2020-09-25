3da07cfddefd24c2be956eaa24a5b2529ed0871f
marciocomp <marcio.comp@gmail.com>
2020-09-25 17:23:14 -0300

    Functions: 1st example. 2nd commit into branch master. By IntelliJ IDEA

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,7 +1,11 @@
 fn main() {
     println!("Hello, world!");
     another_function();
+    new_function();
 }
 fn another_function(){
     println!("Another function.");
+}
+fn new_function(){
+    println!("Now I'm using branch to commit on this git project!")
 }
