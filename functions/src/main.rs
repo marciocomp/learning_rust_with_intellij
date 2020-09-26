2701c18035fdfe5e04aeb83d6ff268ee71fdf292
marciocomp <marcio.comp@gmail.com>
2020-09-26 14:26:21 -0300

    testing .gitignore

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,13 +1,20 @@
 fn main() {
     println!("Hello, world!");
     another_function();
-    new_function();
+    let a = 10;
+    let b = 20;
+    let c = new_function(a,b);
+    println!("C: {}", c);
 }
 fn another_function(){
     println!("Another function.");// Testing commit to 1s_branch_functions
     println!("Testing branch!");
     println!("New testing branch!");
 }
-fn new_function(){
-    println!("Now I'm using branch to commit on this git project!")
+fn new_function(x: i32, y: i32) -> f64{
+    println!("Now I'm using branch to commit on this git project!");
+    println!("Testing function");
+    let z: f64 = (x as f64) /(y as f64);
+    z
+
 }
