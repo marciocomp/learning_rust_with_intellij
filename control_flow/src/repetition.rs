7c374c10d840084689d3226a4b82149cc85edfdf
marciocomp <marcio.comp@gmail.com>
2020-09-26 17:38:50 -0300

    Control flow: Final commit -> all repetition examples. commit in control_flow_repetition

--- control_flow/src/repetition.rs
+++ control_flow/src/repetition.rs
@@ -4,7 +4,12 @@ pub fn run(){
     // }
     println!("The result is: {}", first(20));
     second();
-  }
+    third();
+    println!("***********");
+    fourth();
+    println!("***********");
+    fifth();
+}
 fn first(breaking:i32) -> i32{
     let mut count = 0;
 
@@ -25,3 +30,29 @@ fn second(){
     }
     println!("LIFTOFF");
 }
+fn third(){
+    let a = [10,20,30,40,50];
+    let mut i = 0;
+
+    while i < 5{
+        println!("The value a[{}] is: {}",i,a[i]);
+        i +=1;
+    }
+}
+fn fourth(){
+    let a = [10,20,30,40,50];
+    let mut i = 0;
+    for element in a.iter(){
+        println!("The value a[{}]is: {}",i, element);
+        i +=1;
+    }
+}
+fn fifth(){
+    let a = [10,20,30,40,50];
+
+    for number in (0..5).rev(){
+        println!("The value a[{}] is: {}", number,a[number]);
+    }
+}
+
+
