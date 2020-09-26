dcdd1027314258296401b578da79702ced95106c
marciocomp <marcio.comp@gmail.com>
2020-09-26 14:52:51 -0300

    Functions: 2nd example. 1st_branch_function

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -5,6 +5,18 @@ fn main() {
     let b = 20;
     let c = new_function(a,b);
     println!("C: {}", c);
+    let d = {
+        let a = 50;
+        a + 1
+    };
+    let a= five();
+    println!("A: {}",a);
+    println!("D: {}",d);
+    let a = plus_one(five());
+    let mut b = plus_one(a);
+    b=plus_one(b);
+    println!("A: {}",a);
+    println!("B: {}",b);
 }
 fn another_function(){
     println!("Another function.");// Testing commit to 1s_branch_functions
@@ -17,4 +29,10 @@ fn new_function(x: i32, y: i32) -> f64{
     let z: f64 = (x as f64) /(y as f64);
     z
 
+}
+fn five()-> i32{
+    5
+}
+fn plus_one(x:i32) -> i32{
+    x + 1
 }
